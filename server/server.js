bfa480450d84221273cb3427f0fd9de1bd155c97
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-10-25 16:22:05 -0500

    Added production statement in server js

--- server/server.js
+++ server/server.js
@@ -19,11 +19,12 @@ app.use("/api/", userRoutes);
 app.use("/api/", imageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen( PORT, () => {
     console.log(`Listening on localhost:${PORT}`)
